f9a614828b0abddf755efa9b6deb70b73ef8a145
daniml3 <danimoral1001@gmail.com>
2020-10-13 17:49:09 +0200

    davinci: import hardware/google/pixel as a soong namespace
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- device.mk
+++ device.mk
@@ -342,7 +342,8 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
